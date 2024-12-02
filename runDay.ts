9fd3f8090f15866956d119decf80b2f6593ddd5b
Bill Bloebaum <bb45459@gmail.com>
2024-12-01 20:58:24 -0800

    update scripts

--- runDay.ts
+++ runDay.ts
@@ -3,14 +3,15 @@ const dayArg = Deno.args[0];
 const filePath = `./src/day${dayArg}/day.${dayArg}.ts`;
 
 const command = new Deno.Command("deno", {
-    args: [
-        "run",
-        "-A",
-        filePath,
-    ],
+  args: [
+    "run",
+    "-A",
+    filePath,
+  ],
 });
 
 const result = await command.output();
 
 const textDecoder = new TextDecoder();
 console.log(textDecoder.decode(result.stdout));
+console.error(textDecoder.decode(result.stderr));
