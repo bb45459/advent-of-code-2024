4b3765d03c1d6f8c9467eb45fd85f07c61eeb2ba
Bill Bloebaum <bb45459@gmail.com>
2024-12-01 20:56:13 -0800

    add run day script

---
+++ runDay.ts
@@ -0,0 +1,16 @@
+const dayArg = Deno.args[0];
+
+const filePath = `./src/day${dayArg}/day.${dayArg}.ts`;
+
+const command = new Deno.Command("deno", {
+    args: [
+        "run",
+        "-A",
+        filePath,
+    ],
+});
+
+const result = await command.output();
+
+const textDecoder = new TextDecoder();
+console.log(textDecoder.decode(result.stdout));
